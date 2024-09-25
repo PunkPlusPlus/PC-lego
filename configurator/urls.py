6ec1ad9a05a5519fde0c8d0f3d1f3db47b7bd749
niksot9 <spbnam@mail.ru>
2024-09-25 22:12:21 +1000

    added 'api/price'

--- configurator/urls.py
+++ configurator/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home_page'),
-    path('assemblerPC', views.AssemblerPCView.as_view(), name='assembler_PC'),
-    path('computers/', views.PCList.as_view())
+    path('assemblerPC/', views.AssemblerPCView.as_view(), name='assembler_PC'),
+    path('computers/', views.PCList.as_view()),
+    path('api/price', views.get_price)
 ]
