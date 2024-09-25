93a6bd58fea5588e20ca58092c1713c86d145b18
niksot9 <spbnam@mail.ru>
2024-09-25 22:13:26 +1000

    added get_price() + PCList

--- configurator/views.py
+++ configurator/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView, FormView, ListView
 from .forms import ConfiguratorForm
 from django.urls import reverse_lazy
-from configurator.models import AssemblerPC
+from configurator.models import AssemblerPC, CPU, GPU, Motherboard, RAM, StorageDrive, PowerSupply, CoolingSystem, Case
+from django.http import HttpResponse
 
 
 class HomePageView(TemplateView):
@@ -15,11 +15,29 @@ class AssemblerPCView(FormView):
     success_url = reverse_lazy('home_page')
 
     def form_valid(self, form):
-        form.save()
         return super().form_valid(form)
 
     def form_invalid(self, form):
         pass
+
+
+def get_price(request):
+    model = request.GET.get('model')
+    id = request.GET.get('id')
+    models_components = {
+        'cpu': CPU,
+        'gpu': GPU,
+        'motherboard': Motherboard,
+        'ram': RAM,
+        'storage_drive': StorageDrive,
+        'power_supply': PowerSupply,
+        'cooling_system': CoolingSystem,
+        'case': Case,
+    }
+    if model in models_components:
+        price = models_components[model].objects.get(id=id).price
+        return HttpResponse(price)
+    return HttpResponse('Компонент не найден', status=400)
 
 
 class PCList(ListView):
