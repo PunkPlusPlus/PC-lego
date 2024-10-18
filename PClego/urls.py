744bbbce2a879cf8bc99a7e2356e686225863a9d
1PerC3nt <myamsar@yandex.ru>
2024-10-18 23:06:58 +1000

    added login form

--- PClego/urls.py
+++ PClego/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('configurator.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('accounts/', include("django.contrib.auth.urls"))
 ] + debug_toolbar_urls()
