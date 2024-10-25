f749372c29eaabcafc23aeb75922b7e6f91e4ba0
niksot9 <spbnam@mail.ru>
2024-10-26 09:42:22 +1000

    update for merging

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Users(models.Model):
-    username = models.CharField('Имя', max_length=20)
-    email = models.EmailField('Почта', max_length=20)
-    password = models.TextField('Пароль', max_length=20)
-
-    def __str__(self):
-        return (f'Name: {self.name}')
