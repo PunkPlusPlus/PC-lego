003e827f0f19d01049f1f5e5e292cea2b704f558
niksot9 <spbnam@mail.ru>
2024-10-26 09:54:38 +1000

    update for merging

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ from django.urls import reverse_lazy
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
-    # success_url = reverse_lazy('login')
+    success_url = reverse_lazy('login')
 
 
     def form_valid(self, form):
